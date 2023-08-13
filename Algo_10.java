6ffe3fc6ee525565b272ab6185380babc67e9e64
Thisara Rathnapala <thisaratr94@gmail.com>
2023-08-14 01:08:38 +0530

    Algo_10 Corrected

--- Algo_10.java
+++ Algo_10.java
@@ -4,7 +4,7 @@ public class Algo_10 {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+
         String input;
         boolean validity = true;
         do {
@@ -15,39 +15,35 @@ public class Algo_10 {
                 validity = false;
             } else {
                 char[] charArray = input.toCharArray();
+
                 for (int i = 0; i < charArray.length; i++) {
-                    if (!Character.isDigit(charArray[i])) {
-                        validity = false;
-                        if (charArray[i] == '-' | charArray[i] == ' ' | charArray[i] == '+') {
-                            validity = true;
-                            break;
-                        }
+                    validity = Character.isDigit(charArray[i]) ? true : charArray[i] == '-' ? true : charArray[i] == ' ' ? true : charArray[i] == '+' ? true :  false;
+                    if (!validity) {
                         System.out.println("Invalid Content");
                         break;
-                    } else validity = true;
+                    }
                 }
             }
         } while (!validity);
 
-        
         String[] number;
         if (input.startsWith("+94")) {
             number = input.split(" ");
-            if (number[0].length() != 3 ||
-                    number[1].length() != 2 ||
-                    number[2].length() != 3 ||
-                    number[3].length() != 4)
+            if (number[1].length() != 2 || number[1].contains("+") || number[1].contains("-") ||
+                number[2].length() != 3 || number[2].contains("+") || number[2].contains("-") ||
+                number[3].length() != 4 || number[3].contains("+") || number[3].contains("-"))
                 System.out.println("Invalid phone number.");
             else
                 System.out.println("Number is validated.");
         } else if (input.startsWith("0")) {
             number = input.split("-");
-            if (number[0].length() != 3 ||
-                    number[1].length() != 7)
+            if (number[0].length() != 3 || number[0].contains("+") || number[0].contains(" ") ||
+                number[1].length() != 7 || number[1].contains("+") || number[1].contains(" "))
                 System.out.println("Invalid phone number.");
             else
                 System.out.println("Number is validated.");
-        } else System.out.println("Invalid phone number.");
+        } else
+            System.out.println("Invalid phone number.");
 
     }
 }
